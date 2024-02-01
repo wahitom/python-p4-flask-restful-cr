4f732038dbf3255cb4de31cb48537c8066a8a126
Miriam Wahito Maina <miriammaina51@gmail.com>
2024-02-01 14:36:27 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,8 +15,80 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-    pass
+class Index(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "index": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(Index, '/')
+
+class Newsletters(Resource):
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
+        return response
+
+# newsletters/app.py
+
+    def post(self):
+        data = request.get_json()
+
+        if not data or 'title' not in data or 'body' not in data:
+            response_dict = {"message": "Invalid request data"}
+            return make_response(jsonify(response_dict), 400)
+
+        new_record = Newsletter(
+            title=data['title'],
+            body=data['body'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+
+api.add_resource(Newsletters, '/newsletters')
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
+        return response
+
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
